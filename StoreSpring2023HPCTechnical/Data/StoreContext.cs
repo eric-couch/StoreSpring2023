24ed0053c474c653e4d6030ab6260e1005808314
Eric Couch <ericcouch@yahoo.com>
2023-03-07 15:23:29 -0600

    Add project files.

---
+++ StoreSpring2023HPCTechnical/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StoreSpring2023HPCTechnical.Models;
+
+namespace StoreSpring2023HPCTechnical.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StoreSpring2023HPCTechnical;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ StoreSpring2023HPCTechnical/Program.cs
@@ -0,0 +1,13 @@
+﻿using StoreSpring2023HPCTechnical.Data;
+using StoreSpring2023HPCTechnical.Models;
+
+using StoreContext context = new StoreContext();
+
+var products = from product in context.Products
+               where product.Price > 10.00M
+               orderby product.Name
+               select product;
+foreach (var product in products)
+{
+    Console.WriteLine($"Id:\t{product.Id}\nName:\t{product.Name}\nPrice:\t{product.Price}");
+}
